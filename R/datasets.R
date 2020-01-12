7b6364170e59924c2f03fcc281b4a3dfa90e9768
eleonoraarnone <eleonora.arnone@polimi.it>
2020-01-12 14:25:13 +0100

    Doc changes in smoothFPCA.R

--- R/datasets.R
+++ R/datasets.R
@@ -1,8 +1,9 @@
 #' Hub mesh
 #'
-#'A triangular mesh of a manifold representing a hub geometry.This is a \code{MESH.2.5D} object created with \code{create.MESH.2.5D}.
+#'The nodes and connectivity matrix of a triangular mesh of a manifold representing a hub geometry. 
+#'This is a \code{MESH.2.5D} object created with \code{create.MESH.2.5D}.
 #'
-#'@name hub
+#'@name hub2.5D
 NULL
 
 
